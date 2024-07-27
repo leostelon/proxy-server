f6f02bbb1e3869b8e4eb19175f26db6094401573
leostelon <leostelon17@gmail.com>
2024-07-27 17:19:15 +0530

    added PATCH method

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const server = http.createServer((req, res) => {
   if (req.method === 'OPTIONS') {
     res.writeHead(204, {
       'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS, PATCH',
       'Access-Control-Allow-Headers': 'Content-Type, Authorization',
       'Access-Control-Allow-Credentials': 'true',
       'Content-Length': '0'
